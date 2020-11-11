c10123de8e01e2a7a11be3227b639fdb2faeaeda
Fausto Alves <fausto.tna@hotmail.com>
2020-11-11 15:23:10 -0300

    Projeto backend criado

---
+++ backend/gs-processos-bk/src/main/java/com/softplan/gestao/GsProcessosBkApplication.java
@@ -0,0 +1,13 @@
+package com.softplan.gestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsProcessosBkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsProcessosBkApplication.class, args);
+	}
+
+}
